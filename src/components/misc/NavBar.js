2ff6c31888fef77c9e84c30f0801486ef93bb005
Patricia Defez <ptddds@gmail.com>
2019-06-05 19:39:47 +0200

    NavBar

--- src/components/misc/NavBar.js
+++ src/components/misc/NavBar.js
@@ -1,32 +1,26 @@
 import React, { Component } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <Link className="navbar-brand" to="/">Navbar</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                    </ul>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark w-100">
+            <a className="navbar-brand" href="https://www.laliga.es/" target="_blank">
+                <img src="https://www.thesportsdb.com/images/media/league/badge/7onmyv1534768460.png" width="100" height="100" alt="logo_LaLiga"/>
+            </a>
+
+            <div className="collapse navbar-collapse " id="mainNavbar">
+            <ul className="navbar-nav my-2 my-lg-0">                                  
+                            <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
+                            </li>
+                            <li className="nav-item">
+                            <NavLink className="nav-link" activeClassName="active" to="/users">Usuarios</NavLink>
+                            </li>           
+                </ul>
                 </div>
-            </nav>
+        </nav>
         );
     }
 }
